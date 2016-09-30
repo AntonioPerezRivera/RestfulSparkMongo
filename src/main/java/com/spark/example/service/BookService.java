28e1e7d5718a10e5bbcec7a1854e3c0624471a61
amperez.rivera <amperez.rivera@atsistemas.com>
2016-09-30 11:55:01 +0200

    Añadido servicio de libro

---
+++ src/main/java/com/spark/example/service/BookService.java
@@ -0,0 +1,46 @@
+package com.spark.example.service;
+ 
+import com.google.gson.Gson;
+import com.mongodb.*;
+import com.spark.example.model.Book;
+import com.spark.example.model.User;
+
+import org.bson.types.ObjectId;
+ 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+ 
+public class BookService {
+ 
+    private final DB db;
+    private final DBCollection collection;
+ 
+    public BookService(DB database) {
+        this.db = database;
+        this.collection = db.getCollection("books");
+    }
+ 
+    public List<User> findAll() {
+        List<User> books = new ArrayList<>();
+        DBCursor dbObjects = collection.find();
+        while (dbObjects.hasNext()) {
+            DBObject dbObject = dbObjects.next();
+            System.out.println(new User((BasicDBObject) dbObject));
+            books.add(new User((BasicDBObject) dbObject));
+        }
+        return books;
+    }
+ 
+    public void createNewUser(String body) {
+        Book book = new Gson().fromJson(body, Book.class);
+        collection.insert(new BasicDBObject("title", book.getTitle())
+        		.append("author", book.getAuthor())
+        		.append("isbn", book.getIsbn())
+        		.append("createdOn", new Date()));
+    }
+ 
+    public Book find(String id) {
+        return new Book((BasicDBObject) collection.findOne(new BasicDBObject("_id", new ObjectId(id))));
+    }
+}
