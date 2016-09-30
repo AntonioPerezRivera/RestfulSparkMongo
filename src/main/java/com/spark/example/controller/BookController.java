416fb5016e2907c0a6735eaba23023dfb1fef8ce
amperez.rivera <amperez.rivera@atsistemas.com>
2016-09-30 11:58:01 +0200

    Corregido controlador de libro

---
+++ src/main/java/com/spark/example/controller/BookController.java
@@ -0,0 +1,35 @@
+package com.spark.example.controller;
+ 
+import com.spark.example.JsonTransformer;
+import com.spark.example.service.BookService;
+ 
+import static spark.Spark.get;
+import static spark.Spark.post;
+ 
+public class BookController {
+ 
+    private static final String CONTEXT = "spark";
+ 
+    private final BookService bookService;
+ 
+    public BookController(BookService bookServ) {
+        this.bookService = bookServ;
+        setupEndpoints();
+    }
+ 
+    private void setupEndpoints() {
+    	
+        post(CONTEXT + "/book", "application/json", (request, response) -> {
+            bookService.createNewBook(request.body());
+            response.status(201);
+            return response;
+        }, new JsonTransformer());
+ 
+        get(CONTEXT + "/book/:id", "application/json", (request, response)
+        		-> bookService.find(request.params(":id")), new JsonTransformer());
+ 
+        get(CONTEXT + "/book", "application/json", (request, response)
+        		-> bookService.findAll(), new JsonTransformer());
+    }
+ 
+}
